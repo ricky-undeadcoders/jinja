48f1e8e1d6c6bbfc29ca2d71261a72aab7765649
Ricky <ricky.whitaker@undeadcodersociety.com>
2018-06-30 14:32:43 -0500

    added some notes

--- ricky_test/749/tester.py
+++ ricky_test/749/tester.py
@@ -1,23 +1,16 @@
 from jinja2 import Environment
-
 env = Environment(lstrip_blocks=False, trim_blocks=False)
-
 str7481 = '''
-    {%+ if True %}
-    foo
-    {% endif %}'''
-
+    <!-- 1 -->
+    {%+ if True %}<!-- 2 -->
+    foo<!-- 3 -->
+    {% endif %}<!-- 4 -->
+    <!-- 5 -->'''
 str7482 = '''
     <!-- 1 -->
     {% if True %}<!-- 2 -->
     foo<!-- 3 -->
     {%+ endif %}<!-- 4 -->
     <!-- 5 -->'''
-
-'''
-When lstrip_blocks == False: jinja2.exceptions.TemplateSyntaxError: tag name expected
-'''
-
 print(env.from_string(str7481).render())
 print(env.from_string(str7482).render())
-
